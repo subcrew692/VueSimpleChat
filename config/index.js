ed55ecd425de432675dcbcd1f04dcc59f494eba4
subcrew692 <constantine61732838250@gmail.com>
2019-02-23 13:48:06 +0800

    after npm build

--- config/index.js
+++ config/index.js
@@ -38,12 +38,12 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../index.html'), //之前是 '../dist/index.html'
+    index: path.resolve(__dirname, '../dist/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../'), // 之前是 '../dist'
+    assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: './', // 之前是 '/'
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
