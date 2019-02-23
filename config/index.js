aa94703ca9fcafb69d473c6c24b507c5d1d84fad
subcrew692 <constantine61732838250@gmail.com>
2019-02-23 13:39:05 +0800

    fix gitignore

--- config/index.js
+++ config/index.js
@@ -38,12 +38,12 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../index.html'), //之前是 '../dist/index.html'
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../'), // 之前是 '../dist'
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './', // 之前是 '/'
 
     /**
      * Source Maps
